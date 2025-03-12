d8c35e5dbfe43fbb1794fd2d0e8f0197076fb503
Andreas Yankopolus <andreas@yank.to>
2025-03-12 14:50:46 -0400

    Output DNS name of load balancer.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
-# Output values
+# Output values.
 
 # Output useful information after deployment.
 
@@ -10,4 +10,9 @@ output "ecs_cluster_name" {
 output "ecs_service_name" {
   description = "Name of the ECS service"
   value       = aws_ecs_service.app.name
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the load balancer"
+  value       = aws_lb.app.dns_name
 }
